aa4a64e012b43cc1806fa04ad4be7924e63d4e30
jinmidnight01 <jinmidnight01@gmail.com>
2024-07-26 03:24:15 +0900

    webcam true

--- iampay_guest/src/pages/SignupPage/SignupPage.js
+++ iampay_guest/src/pages/SignupPage/SignupPage.js
@@ -37,7 +37,7 @@ const SignupPage = () => {
 
   // 웹캠 설정
   const [user_face_img, setUserFaceImg] = useState(cameraLogo);
-  const [permissionsGranted, setPermissionsGranted] = useState(false);
+  const [permissionsGranted, setPermissionsGranted] = useState(true);
   const [error, setError] = useState(null);
   const width = 300;
   const height = 300;
