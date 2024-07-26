2b615c8dcaa3ad838ede0b5c7933750f05e54c3d
jinmidnight01 <jinmidnight01@gmail.com>
2024-07-26 14:50:31 +0900

    css

--- iampay_guest/src/pages/NotFoundPage/NotFoundPage.js
+++ iampay_guest/src/pages/NotFoundPage/NotFoundPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Button from '../../components/Button';
 import styles from '../../css/NotFoundPage.module.css';
+import Footer from '../../components/Footer';
 
 const NotFoundPage = () => {
   let link = "";
@@ -18,6 +19,7 @@ const NotFoundPage = () => {
         해당 주소는 <br/> 존재하지 않는 페이지입니다
       </div>
       <Button buttonLink={link} buttonColor="#FF5555" buttonText="홈으로" />
+      <Footer />
     </div>
   );
 };
