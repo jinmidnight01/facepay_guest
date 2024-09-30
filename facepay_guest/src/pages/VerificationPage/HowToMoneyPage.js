9caaf978b87218391ea0ea7ae5d9cb8c972c8745
jinmidnight01 <jinmidnight01@gmail.com>
2024-09-30 13:37:59 +0900

    money text

--- facepay_guest/src/pages/VerificationPage/HowToMoneyPage.js
+++ facepay_guest/src/pages/VerificationPage/HowToMoneyPage.js
@@ -38,7 +38,7 @@ const HowToMoneyPage = () => {
       {/* 첫 번째 가이드 */}
       <div className={styles.firstGuideBox}>
         <div className={styles.guide}>
-          <span>계좌/카드</span> 추후 연동 예정
+          <span>계좌/카드</span>는 추후 연동 예정
         </div>
       </div>
 
