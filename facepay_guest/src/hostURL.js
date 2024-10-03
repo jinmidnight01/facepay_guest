b12f737c7d82f2e6acb5e1d454f2a8f309384561
jinmidnight01 <jinmidnight01@gmail.com>
2024-10-03 23:46:26 +0900

    hostulr

--- facepay_guest/src/hostURL.js
+++ facepay_guest/src/hostURL.js
@@ -1,5 +1,6 @@
 // const hostURL = "http://localhost:8001";
 // const hostURL = "http://127.0.0.1:8000";
-const hostURL = "https://refeat.ai";
+// const hostURL = "https://refeat.ai";
+const hostURL = "https://facepayapp.com";
 
 export default hostURL;
