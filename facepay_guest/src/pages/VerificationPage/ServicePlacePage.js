5a3863643f335060b9efcfdaf0dcc000c01cac29
jinmidnight01 <jinmidnight01@gmail.com>
2024-11-15 23:18:27 +0900

    guest

--- facepay_guest/src/pages/VerificationPage/ServicePlacePage.js
+++ facepay_guest/src/pages/VerificationPage/ServicePlacePage.js
@@ -2,8 +2,12 @@ import { React, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../../css/ServicePlacePage.module.css";
 import Header from "../../components/Header";
+import axios from "axios";
+import hostURL from "../../hostURL";
+import Loading from "../../components/Loading";
 
 const ServicePlacePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const output = useLocation().state;
   const navigate = useNavigate();
 
@@ -25,50 +29,66 @@ const ServicePlacePage = () => {
     };
   }, [navigate, output]);
 
+  const handleClick = () => {
+    setIsLoading(true);
+
+    axios
+      .patch(`${hostURL}/api/users/${output.userID}`, {
+        verified: true,
+      })
+      .then((res) => {
+        navigate("/");
+        alert("등록이 완료되었습니다");
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div>
       <Header logoLink="/" />
-      <div className={styles.modalTitle}>시범 서비스 매장 목록</div>
-
-      {/* 첫 번째 가이드 */}
-      <div className={styles.firstGuideBox}>
-        <div className={styles.guide}>아지트</div>
-      </div>
-
-      {/* 두 번째 가이드 */}
-      <div
-        className={`${styles.guideBox} ${fadeInStep >= 1 ? styles.fadeIn : ""}`}
-        style={{marginBottom: `15px`}}
-      >
-        <div className={styles.guide}>
-          서문 첫집
+      {isLoading ? (
+        <div className={styles.loadingBox}>
+          <Loading />
+          <div className={styles.signupLoadingBox}>
+            <div className={styles.signupLoading}>로딩 중 ...</div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className={styles.modalTitle}>시범 서비스 매장 목록</div>
 
-      {/* 세 번째 가이드 */}
-      {/* <div
-        className={`${styles.guideBox} ${fadeInStep >= 2 ? styles.fadeIn : ""}`}
-      >
-        <div className={styles.guide}>
-          카페 서문
-        </div>
-      </div> */}
+          {/* 첫 번째 가이드 */}
+          <div className={styles.firstGuideBox}>
+            <div className={styles.guide}>아지트</div>
+          </div>
 
-      {/* 마지막 버튼 */}
-      <div
-        className={`${styles.nextBox} ${fadeInStep >= 2 ? styles.fadeIn : ""}`}
-      >
-        <div className={styles.buttonBox}>
-          <button
-            onClick={() => {
-              navigate("/event", { state : output });
-            }}
-            className={styles.button}
+          {/* 두 번째 가이드 */}
+          <div
+            className={`${styles.guideBox} ${
+              fadeInStep >= 1 ? styles.fadeIn : ""
+            }`}
+            style={{ marginBottom: `15px` }}
           >
-            확인헀습니다
-          </button>
+            <div className={styles.guide}>서문 첫집</div>
+          </div>
+
+          {/* 마지막 버튼 */}
+          <div
+            className={`${styles.nextBox} ${
+              fadeInStep >= 2 ? styles.fadeIn : ""
+            }`}
+          >
+            <div className={styles.buttonBox}>
+              <button onClick={handleClick} className={styles.button}>
+                가입 완료
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
